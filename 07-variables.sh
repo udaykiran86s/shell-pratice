4e0e2b94543a64ce093bb97a6d8e1a93a420c937
unknown <samanthulaudaykiran@gmail.com>
2026-04-20 12:41:11 +0530

    messages

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,4 +6,4 @@ start_time=$(date +%s)
 sleep 10 &
 end_time=$(date +%s)
 total_time=$($end_time-$start_time)
-echo " script executed in $total _time in seconds"
+echo " script executed in $total_time in seconds"
