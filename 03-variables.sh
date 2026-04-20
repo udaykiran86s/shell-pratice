97bb05daf971e31b5b3b92e05b2f3554061f1ec4
unknown <samanthulaudaykiran@gmail.com>
2026-04-20 11:15:44 +0530

    messages

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+
+PERSON1=UDAY
+PERSON2=KIRAN
+echo "$PERSON1: hi'
+echo "$PERSON2 :: hello rmesh"
+echo "$PERSON1 :: what are you learning recently"
+echo "$PERSON2:: shell scripting"
