8e7be7162bc8ead415c710d4b32a92d59a8a91a4
unknown <samanthulaudaykiran@gmail.com>
2026-04-20 20:22:15 +0530

    messages

--- 16-logs.sh
+++ 16-logs.sh
@@ -5,7 +5,10 @@ r="\e[31m"
 g="\e[32m"
 y="\e[33m"
 n="\e[0m"
-
+if [ $USERID -ne 0 ];then
+    echo "please run with root user"
+    exit 1
+fi
 LOGS_FOLDER="/var/log/shel-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -13,12 +16,9 @@ mkdir -p $LOGS_FOLDER
 
 echo "Script started executed at: $(date)"
 
-if [ $USERID -ne 0 ];then
-    echo "please run with root user"
-    exit 1
-fi
 
-VALIDATE(){
+
+VALIDATE() {
     if [ $1 -ne 0 ] ; then
     echo "installation $r failed $n need root permission"
     exit 1
