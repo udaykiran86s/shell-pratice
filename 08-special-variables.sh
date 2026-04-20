faa7295fa3184fcaa61fea840c7c098186290f47
unknown <samanthulaudaykiran@gmail.com>
2026-04-20 13:07:13 +0530

    messages

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,6 @@
 echo "all variables passwed to the script : $@"
 echo "all varaiable passed to the script : $*"
 echo " scriptname : $0"
+echo "current directory  : $pwd"
+echo "who is running this : $user"
+echo " current user :$user"
