e062ea4490a8831915225f5f2f3e57453f57a753
unknown <samanthulaudaykiran@gmail.com>
2026-04-20 12:05:59 +0530

    messages

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-
+#argumnets
 PERSON1=$1
 PERSON2=$2
 
---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo " please enter pin number"
+read PIN_number # here pin_number is variable
+
+echo "PIN_NUMBER " IS $PIN_NUMBER"
