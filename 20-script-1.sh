b76de02c7ddc1e43dfc6bb3481724e492dc9cae9
unknown <samanthulaudaykiran@gmail.com>
2026-04-22 22:36:44 +0530

    messages

---
+++ 20-script-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+NAME=INDIA
+echo "My country: $NAME"
+echo "PID OF SCRIPT-1: $$"
+sh 21-script-2.sh
---
+++ 21-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+NAME=INDIA
+echo "My country: $NAME"
+echo "PID OF SCRIPT-2: $$"
